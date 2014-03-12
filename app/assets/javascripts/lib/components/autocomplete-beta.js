40826e8d5253ca77dade20efc1a14227f7e16d6b
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-03-12 13:55:38 -0500

    trimmed out some jQuery, removed pub/sub features, and reorganized code

--- app/assets/javascripts/lib/components/autocomplete-beta.js
+++ app/assets/javascripts/lib/components/autocomplete-beta.js
@@ -5,17 +5,15 @@ define([ "jquery" ], function($) {
 
   "use strict";
 
-  // @args = {}
-  // $el: {string} selector for parent element
   var Autocomplete = function(args) {
 
     // create a new results variable
     this.results = [];
 
-    // set the listenerID
-    this.listenerID = this._createRandomID();
-    this.listener = "#" + this.listenerID;
+    // set the selected index to begin
+    this.selectedResultIndex = 0;
 
+    // defaults to be overwritten by passed args
     this.config = {
       el: "",
       wrapID: "autocomplete-wrapper",
@@ -23,143 +21,94 @@ define([ "jquery" ], function($) {
       highlightClass: "highlight",
       limit: 10,
       threshold: 2,
-
-      onItem: this._defaultOnItem,
-      template: this._defaultTemplate
+      onItem: this._defaultOnItem, // pass selectedEl and main el
+      template: this._defaultTemplate // pass results array
     };
 
+    // overwrite defaults with args
     $.extend(this.config, args);
 
-    this.$el = this.config.el;
+    // initialize
     this.init();
   };
 
   Autocomplete.prototype.init = function() {
 
-    // create a listener object to attach to (random string);
-    this._createListener();
-
     // wrap el in some tags
     this._wrapAutocomplete();
 
-    // setup listeners and broadcasters
-    this.listen();
-    this.broadcast();
-
-  };
-
-  // -------------------------------------------------------------------------
-  // Subscribe to Events
-  // -------------------------------------------------------------------------
-  Autocomplete.prototype.listen = function() {
-    var _this = this;
-
-    // listen for :typing and fetch results with search term
-    $(_this.listener).on(":typing", function(e, searchTerm) {
-      // fetch results
-      _this._fetchResults(searchTerm);
-    });
-
-    $(_this.listener).on(":resultsfetched", function() {
-      _this.selectedIndex = 0;
-      _this._displayResults();
-    });
-
-    $(_this.listener).on(":itemselected", function(e, data) {
-      console.log(_this.selectedIndex);
-      // get ID of whichever <li> is the selected Index
-      var selectedID = $("#" + _this.config.resultsID + " ul li").eq(_this.selectedIndex).attr("ID");
-
-      // call the onItem function, passing the ID of that index, as well as the el of the app
-      _this.config.onItem(selectedID, _this.config.el);
-
-      // finally, clear results, because the onItem function is now taking over
-      _this._clearResults();
-    });
-
-    $(_this.listener).on(":indexchanged", function() {
-      // when the index has changed, this allows us to perform some function (like highlighting)
-      _this._highlightIndex();
-    });
-
-  };
-
-  // -------------------------------------------------------------------------
-  // Broadcast Events
-  // -------------------------------------------------------------------------
-  Autocomplete.prototype.broadcast = function() {
-    var _this = this;
-
-    // broadcast user events from the wrapper
-    $("#" + this.config.wrapID).on("keyup", function(e) {
-      _this._processTyping(e);
-    });
-
-    // listen for clicks on autocomplete options
-    $("#" + this.config.resultsID).on("click hover", "ul li", function(e) {
-      e.preventDefault();
-      _this.selectedIndex = $(e.target).index();
-      // call onItem when a list item is clicked
-      $(_this.listener).trigger(":itemselected", e);
-    });
+    // setup DOM handlers
+    this._handlers();
 
   };
 
   // -------------------------------------------------------------------------
   // Private Functions
   // -------------------------------------------------------------------------
-  Autocomplete.prototype._createListener = function() {
-    $("body").prepend("<div id='" + this.listenerID + "' />");
-    return;
+  Autocomplete.prototype._handlers = function() {
+    var _this = this;
+
+    // broadcast user events from the wrapper
+    $("#" + this.config.wrapID).on("keyup", function(e) {
+      _this._processTyping(e);
+      if (_this.resultsBeingDisplayed) {
+        switch (e.keyCode) {
+          case 38: {// up
+            e.preventDefault();
+            _this._changeIndex("up");
+            break;
+          }
+          case 40: {// down
+            e.preventDefault();
+            _this._changeIndex("down");
+            break;
+          }
+          case 13: {// enter
+            e.preventDefault();
+            _this._itemSelected();
+            break;
+          }
+          case 27: {// esc
+            e.preventDefault();
+            _this._clearResults();
+            break;
+          }
+          default: {//default
+            _this._processTyping(e);
+            break;
+          }
+        }
+      } else {
+        _this._processTyping(e);
+      }
+    });
+
+    // listen for clicks on autocomplete options
+    $("#" + this.config.resultsID).on("click", "ul li", function(e) {
+      e.preventDefault();
+      _this.selectedResultIndex = $(e.target).index();
+      // call onItem when a list item is clicked
+      _this._itemSelected();
+    });
+
   };
 
-  Autocomplete.prototype._createRandomID = function() {
-    var x = "b" + Math.random().toString(36).substring(7);
-    return x;
+  Autocomplete.prototype._itemSelected = function() {
+    // get ID of whichever <li> is the selected Index
+    var selectedEl = $("#" + this.config.resultsID + " ul li").eq(this.selectedResultIndex);
+    // call the onItem function, passing the ID of that index, as well as the el of the app
+    this.config.onItem(selectedEl[0], this.config.el);
+    // finally, clear results, because the onItem function is now taking over
+    this._clearResults();
   };
 
   Autocomplete.prototype._processTyping = function(e) {
-    if (_this.resultsBeingDisplayed) {
-      switch (e.keyCode) {
+    var searchTerm = e.target.value;
 
-        case 38: {// up
-          e.preventDefault();
-          _this._changeIndex("up");
-          break;
-        }
-        case 40: {// down
-          e.preventDefault();
-          _this._changeIndex("down");
-          break;
-        }
-        case 13: {// enter
-          e.preventDefault();
-          $(_this.listener).trigger(":itemselected", e);
-          break;
-        }
-        case 27: {// esc
-          e.preventDefault();
-          _this._clearResults();
-          break;
-        }
-        default: {//default
-          _this._processTyping(e);
-          break;
-        }
-      }
+    if (searchTerm.length >= this.config.threshold) {
+      this._fetchResults(searchTerm);
     } else {
-
-      var searchTerm = e.target.value;
-
-      if (searchTerm.length >= this.config.threshold) {
-
-        $(this.listener).trigger(":typing", searchTerm);
-
-      } else {
-
-        this._clearResults();
-
-      }
+      this._clearResults();
     }
 
   };
@@ -168,21 +117,22 @@ define([ "jquery" ], function($) {
     var resultCount = $("#" + this.config.resultsID + " ul li").length,
         changed = false;
 
-    if (direction === "up" && this.selectedIndex > 0) {
-      this.selectedIndex--;
+    if (direction === "up" && this.selectedResultIndex > 0) {
+      this.selectedResultIndex--;
       changed = true;
-    } else if (direction === "down" && this.selectedIndex < resultCount - 1) {
-      this.selectedIndex++;
+    } else if (direction === "down" && this.selectedResultIndex < resultCount - 1) {
+      this.selectedResultIndex++;
       changed = true;
     }
-    if (changed) {
-      $(this.listener).trigger(":indexchanged");
+
+    if (changed === true) {
+      this._highlightIndex();
     }
   };
 
   Autocomplete.prototype._highlightIndex = function() {
     $("." + this.config.highlightClass).removeClass(this.config.highlightClass);
-    $("#" + this.config.resultsID + " ul li").eq(this.selectedIndex).addClass(this.config.highlightClass);
+    $("#" + this.config.resultsID + " ul li").eq(this.selectedResultIndex).addClass(this.config.highlightClass);
   };
 
   Autocomplete.prototype._wrapAutocomplete = function() {
@@ -194,6 +144,7 @@ define([ "jquery" ], function($) {
   Autocomplete.prototype._clearResults = function() {
     $("#" + this.config.resultsID).html("").hide();
     this.resultsBeingDisplayed = false;
+    this.selectedResultIndex = 0;
   };
 
   Autocomplete.prototype._displayResults = function() {
@@ -207,7 +158,7 @@ define([ "jquery" ], function($) {
     // assume user-generated function that returns JSON array
     this.results = this.config.fetch(searchTerm);
     // when fetching is done...
-    $(this.listener).trigger(":resultsfetched");
+    this._displayResults();
   };
 
   Autocomplete.prototype._generateResultsList = function() {
@@ -215,6 +166,7 @@ define([ "jquery" ], function($) {
     // create display list to append to html
     var displayList = "<ul>",
         listItems = "";
+
     // call the user's template with the results array
     listItems = this.config.template(this.results);
 
@@ -225,9 +177,10 @@ define([ "jquery" ], function($) {
 
   };
 
-  Autocomplete.prototype._defaultOnItem = function(selectedID, el) {
+  /* These functions should be replaced in the user's instantiation */
+  Autocomplete.prototype._defaultOnItem = function(selectedEl, el) {
     // user should replace this function during instantiation
-    var name = $("#" + selectedID).text();
+    var name = $(selectedEl).attr("data-name");
     $(el).val(name);
   };
 
@@ -239,7 +192,7 @@ define([ "jquery" ], function($) {
         listItems = "";
     // should return an HTML string of list items
     for (i = 0; i < listLength; i++) {
-      listItem = "<li id='item" + i + "'>";
+      listItem = "<li id='item" + i + "' data-name='" + results[i].n + "'>";
       // iterate through each property in the object (ugly on purpose for end user)
       for (var p in results[i]) {
         if (results[i].hasOwnProperty(p)) {
