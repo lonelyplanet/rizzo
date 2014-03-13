070fe6dd740caa63435cdb1c2421406dbce3fa3e
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-03-13 13:06:55 -0500

    started scoping a new spec file.

--- app/assets/javascripts/lib/components/autocomplete-tdd.js
+++ app/assets/javascripts/lib/components/autocomplete-tdd.js
@@ -1,4 +1,4 @@
-define([], function() {
+define([ "jquery" ], function($) {
 
   "use strict";
 
@@ -10,15 +10,7 @@ define([], function() {
       el: ""
     };
 
-    this._extend(this.config, args);
-  };
-
-  AutoComplete.prototype._extend = function(config, args) {
-    for (var key in args) {
-      if (args.hasOwnProperty(key)) {
-        config[key] = args[key];
-      }
-    }
+    $.extend(this.config, args);
   };
 
   return AutoComplete;
