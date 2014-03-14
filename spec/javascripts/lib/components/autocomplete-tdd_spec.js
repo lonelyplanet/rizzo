73a11d32c74a0bfc00be212ae488a8c0969c86b9
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-03-14 11:38:40 -0500

    wrote more tests for the TDD version of the autocomplete and added more functions.

--- spec/javascripts/lib/components/autocomplete-tdd_spec.js
+++ spec/javascripts/lib/components/autocomplete-tdd_spec.js
@@ -19,64 +19,266 @@ require([ "jquery", "public/assets/javascripts/lib/components/autocomplete-tdd.j
 
     describe("The object", function() {
 
-      it("should exist");
+      it("should exist.", function() {
+        expect(tester).toBeDefined();
+      });
 
-      it("should have an config.el attribute.")
+      it("should have a config.el attribute.", function() {
+        expect(tester.config.el).toBeDefined();
+      });
+
+      it("should override the defauls with the user-generated options.", function() {
+        expect(tester.config.el).toEqual("#js-autocomplete-test");
+      });
+
+      it("should have an empty array as the results.", function() {
+        expect(tester.results).toEqual([]);
+      });
 
     });
 
     describe("The DOM element", function() {
 
-      it("should exist.");
+      it("should exist.", function() {
+        var el = $(tester.config.el);
+        expect(el).toExist();
+      });
 
-      it("should be wrapped in a div with the passed ID.");
+      it("should be wrapped in a div with the passed ID.", function() {
+        var el = $(tester.config.el);
+        expect(el.parent("#" + tester.config.wrapID)).toExist();
+      });
 
-      it("should have a HIDDEN results div directly after it.");
+      it("should have a HIDDEN results div directly after it.", function() {
+        var el = $("#" + tester.config.resultsID);
+        expect(el).toExist();
+        expect(el).toHaveClass("is-hidden");
+      });
 
-    });
-
-    describe("The user search string", function() {
-
-      it("should be optimized for searching.");
-
-      it("should be passed to the fetch call.");
-
-    });
-
-    describe("The fetch call", function() {
-
-      it("should receive a search string.");
-
-      it("should return an array.");
-
-      it("should trigger a display of results.");
     });
 
     describe("The display of results", function() {
+      var el;
 
-      it("should return a string of list items.");
+      beforeEach(function() {
+        el = $("#" + tester.config.resultsID);
+      });
 
-      it("should be opened when results are displayed.");
+      afterEach(function() {
+        el = undefined;
+      });
 
-      it("should be closed when esc is pressed.");
+      it("should remove is-hidden class on showResults().", function() {
+        tester.showResultsPanel();
+        expect(el).not.toHaveClass("is-hidden");
+      });
 
-      it("should be closed when a result is selected.");
+      it("should set displayed to true showResults().", function() {
+        tester.hideResultsPanel();
+        tester.showResultsPanel();
+        expect(tester.displayed).toBeTruthy;
+      });
 
-      it("should be closed when the user blurs focus from the input or dropdown.");
+      it("should add is-hidden class on hideResults().", function() {
+        tester.hideResultsPanel();
+        expect(el).toHaveClass("is-hidden");
+      });
 
-      it("should put an 'active' class on the result being navigated to.");
+      it("should clear all html on clearResults.", function() {
+        el.html("<li>content</li>");
+        tester.clearResults();
+        expect(el).toBeEmpty();
+      });
 
-      it("should remove the 'active' class from all other results.");
+      it("should clear the global results array on clearResults.", function() {
+        tester.results = [ 1,2,3 ];
+        tester.clearResults();
+        expect(tester.results).toEqual([]);
+      });
 
-      it("should listen for arrow keys and navigate through results.");
+      it("should hide results panel on clearResults.", function() {
+        tester.showResultsPanel();
+        tester.clearResults();
+        expect(el).toHaveClass("is-hidden");
+      });
 
-      it("should select a result on click.");
-
-      it("should select a result on enter.");
-
-      it("should select a result on tab.");
+      it("should set displayed to false on hide results.", function() {
+        tester.showResultsPanel();
+        tester.hideResultsPanel();
+        expect(tester.displayed).toBeFalsy();
+      });
 
     });
+
+    describe("The user typing", function() {
+
+      it("should not fetch results if searchTerm is blank.", function() {
+        spyOn(tester, "fetchResults");
+        tester.processSearch("");
+        expect(tester.fetchResults).not.toHaveBeenCalled();
+      });
+
+      it("should not fetch results if searchTerm is less than threshold.", function() {
+        tester.config.threshold = 2
+        spyOn(tester, "fetchResults");
+        tester.processSearch("f");
+        expect(tester.fetchResults).not.toHaveBeenCalled();
+      });
+
+      it("should fetch results if searchTerm is greater than threshold.", function() {
+        tester.config.threshold = 2
+        spyOn(tester, "fetchResults");
+        tester.processSearch("fra");
+        expect(tester.fetchResults).toHaveBeenCalled();
+      });
+
+      it("should clear results if the input is empty.", function() {
+        var e = {
+          target: {
+            value: ""
+          }
+        };
+        spyOn(tester, "clearResults");
+        tester.processTyping(e);
+        expect(tester.clearResults).toHaveBeenCalled();
+      });
+
+      it("should processSpecial if there are special keys and results are displayed.", function() {
+        tester.displayed = true;
+        var e = {
+          target: {
+            value: "test"
+          },
+          keyCode: 38 // up arrow
+        };
+        spyOn(tester, "processSpecialKey");
+        tester.processTyping(e);
+        expect(tester.processSpecialKey).toHaveBeenCalled();
+      });
+
+      it("should not processSpecial if there are special keys and results are not displayed.", function() {
+        tester.displayed = false;
+        var e = {
+          target: {
+            value: "test"
+          },
+          keyCode: 38 // up arrow
+        };
+        spyOn(tester, "processSpecialKey");
+        tester.processTyping(e);
+        expect(tester.processSpecialKey).not.toHaveBeenCalled();
+      });
+
+      it("should processSearch if there are no special keys.", function() {
+        var e = {
+          target: {
+            value: "test"
+          },
+          keyCode: 65 // 'a' key
+        };
+        spyOn(tester, "processSearch");
+        tester.processTyping(e);
+        expect(tester.processSearch).toHaveBeenCalled();
+      });
+
+    });
+
+    describe("The fetching of results", function() {
+
+      it("should call the config.fetch method to fetch results.", function() {
+        tester.config.threshold = 2;
+        spyOn(tester.config, "fetch").andCallThrough();
+        tester.fetchResults("fra");
+        expect(tester.config.fetch).toHaveBeenCalled();
+      });
+
+      it("should return an array when fetching.", function() {
+        spyOn(tester.config, "fetch").andCallThrough();
+        var result = tester.fetchResults("fra"),
+            isArray = false;
+        if ( Object.prototype.toString.call( result ) === "[object Array]" ) {
+          isArray = true;
+        }
+        expect(isArray).toBeTruthy();
+      });
+
+      it("should set the global results array.", function() {
+        spyOn(tester.config, "fetch").andReturn([ 1,2,3 ]);
+        var results = tester.fetchResults("fra");
+        expect(tester.results).toEqual([ 1,2,3 ]);
+      });
+
+      it("shouldn't change the global result set if nothing returned.", function() {
+        tester.results = [ 1 ];
+        spyOn(tester.config, "fetch").andReturn([]);
+        tester.fetchResults("fra");
+        expect(tester.results).toEqual([ 1 ]);
+      });
+
+    });
+
+    describe("Rendering results", function() {
+
+      it("should return a string of <li>s inside a <ul>.", function() {
+        spyOn(tester.config, "template").andReturn("<li>test</li>");
+        var list = tester.renderList();
+        expect(list).toEqual("<ul><li>test</li></ul>");
+      });
+
+      it("calling populateResultPanel should fill the resultsID div.", function() {
+        tester.results = [ 1,2,3 ];
+        tester.populateResultPanel();
+        expect($("#" + tester.config.resultsID)).not.toBeEmpty();
+      });
+
+    });
+
+    describe("Navigating results", function() {
+
+      it("should not be able to move up at index 0.", function() {
+        tester.results = [ "a", "b", "c" ];
+        tester.resultIndex = 0;
+        tester.changeIndex("up");
+        expect(tester.resultIndex).toEqual(0);
+      });
+
+      it("should not be able to move down at last item.", function() {
+        tester.results = [ "a", "b", "c" ];
+        tester.resultIndex = 2;
+        tester.changeIndex("down");
+        expect(tester.resultIndex).toEqual(2);
+      });
+
+      it("should move down if not at last item.", function() {
+        tester.results = [ "a", "b", "c" ];
+        tester.resultIndex = 1;
+        tester.changeIndex("down");
+        expect(tester.resultIndex).toEqual(2);
+      });
+
+      it("should move up if not at first item.", function() {
+        tester.results = [ "a", "b", "c" ];
+        tester.resultIndex = 1;
+        tester.changeIndex("up");
+        expect(tester.resultIndex).toEqual(0);
+      });
+
+      it("should return true if changed.", function() {
+        tester.results = [ "a", "b", "c" ];
+        tester.resultIndex = 1;
+        var changed = tester.changeIndex("up");
+        expect(changed).toBeTruthy();
+      });
+
+      it("should return false if not changed.", function() {
+        tester.results = [ "a", "b", "c" ];
+        tester.resultIndex = 0;
+        var changed = tester.changeIndex("up");
+        expect(changed).toBeFalsy();
+      });
+
+    });
+
   });
 
 });
