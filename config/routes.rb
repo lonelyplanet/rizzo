ec43d83a741db23b32c328ec8fb737f668de9161
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-03-21 09:01:04 -0500

    added a modern/* route for exposing the standard rizzo styles (including leaderboard) to sites like wordpress

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rizzo::Application.routes.draw do
   get 'client-solutions/global-body-header' => 'global_resources#show', :defaults => { :snippet => "body_header", :cs => "true" }
   get 'client-solutions/global-body-footer' => 'global_resources#show', :defaults => { :snippet => "body_footer", :cs => "true" }
 
+  # Core for WP
+  get 'modern/global-head'        => 'global_resources#show', :defaults => { :snippet => "head" }
+  get 'modern/global-body-header' => 'global_resources#show', :defaults => { :snippet => "body_header" }
+  get 'modern/global-body-footer' => 'global_resources#show', :defaults => { :snippet => "body_footer" }
+
   # Legacy
   get 'global-head'                  => 'global_resources#show', :defaults => { :snippet => "head" }
   get 'global-head-thorntree'        => 'global_resources#show', :defaults => { :snippet => "head", :suppress_tynt => "true" }
