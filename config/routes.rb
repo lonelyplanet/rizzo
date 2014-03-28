44b214a1b60bb8c0b3952423a8d42f6cdf818c27
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-03-28 12:42:52 -0500

    renamed routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rizzo::Application.routes.draw do
   get 'client-solutions/global-body-footer' => 'global_resources#show', :defaults => { :snippet => "body_footer", :cs => "true" }
 
   # Core for exposing modern layout
-  get 'modern/global-head'        => 'global_resources#show', :defaults => { :snippet => "head" }
-  get 'modern/global-body-header' => 'global_resources#show', :defaults => { :snippet => "body_header" }
-  get 'modern/global-body-footer' => 'global_resources#show', :defaults => { :snippet => "body_footer" }
+  get 'modern-head'        => 'global_resources#show', :defaults => { :snippet => "head" }
+  get 'modern-body-header' => 'global_resources#show', :defaults => { :snippet => "body_header" }
+  get 'modern-body-footer' => 'global_resources#show', :defaults => { :snippet => "body_footer" }
 
   # Legacy
   get 'global-head'                  => 'global_resources#show', :defaults => { :snippet => "head", :legacystyle => "true" }
