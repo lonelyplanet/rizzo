5df35f0127c95e7e213101c0ff35ae29cc25442f
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-04-28 13:16:24 -0500

    borders on autocomplete, icons in bookings dropdown, user menu cleanup

--- app/assets/javascripts/lib/core/nav_search.js
+++ app/assets/javascripts/lib/core/nav_search.js
@@ -23,7 +23,7 @@ define([ "jquery", "autocomplete" ], function($, Autocomplete) {
         resultsWrapper: "<div class='autocomplete'></div>",
         resultsContainer: "<div class='autocomplete__results icon--tapered-arrow-up--after icon--white--after'></div>",
         resultsItemHighlightClass: "autocomplete__results__item--highlight",
-        resultsItem: "<div><a class='autocomplete__results__item icon--{{type}}--before icon--body-grey--before' href='{{slug}}'>{{name}}</a></div>",
+        resultsItem: "<a class='autocomplete__results__item icon--{{type}}--before icon--body-grey--before' href='{{slug}}'>{{name}}</a>",
         searchTermHighlightClass: "autocomplete__search-term--highlight",
         hiddenClass: "is-hidden"
 
