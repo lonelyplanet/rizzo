a18c99f139f92d2e9ab1e9a200f373b405c85464
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-07-22 21:37:24 -0500

    added tests for hashed features. added a default path for anything /layouts/*

--- app/controllers/layout_controller.rb
+++ app/controllers/layout_controller.rb
@@ -12,9 +12,4 @@ class LayoutController < GlobalController
     render "custom_layouts/preview", locals: get_layout_config(:"#{params[:route]}")
   end
 
-  def show_nav_section(section)
-    (defined?(section) && section == true) || (!defined? section)
-  end
-  helper_method :show_nav_section
-
 end
