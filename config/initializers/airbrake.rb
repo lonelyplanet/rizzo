ca8001deba623517823efcce1265740c7dd8456d
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2018-04-10 19:07:30 +0200

    Update Airbrake config to v5
    .api_key is obsolete. Related chef-repo commit:
    https://github.com/lonelyplanet/chef-repo/commit/c82f92b6fab766ac4dd67a2df79b7ee3ec86de9e
    And Community commit:
    https://github.com/lonelyplanet/community/pull/3324/commits/35ceca9154771ace5163f44c44f04f31bde3e9c5

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,6 @@
 if Rails.env.production? && defined?(Airbrake)
   Airbrake.configure do |config|
-    config.api_key = ENV['AIRBRAKE_API_KEY']
+    config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   end
 end
